f52e4f94dc4e02503822164bcc09b327c22927de
Ben Lynn <benlynn@gmail.com>
2019-03-27 23:16:00 -0700

    Empty term terminates program.
    I like this better than the newline convention I had.

--- thc.c
+++ thc.c
@@ -38,13 +38,11 @@ u parse() {
   u outer = run();
   tabn = 0;
   for(;;) {
-    tab[tabn++] = parseTerm();
-    u c = run();
-    switch (c) {
-      case '\n': return heapOn(outer, heapOn(tab[tabn - 1], heapOn('<', 0)));
-      case ';': break;
-      default: die("expected term terminator");
-    }
+    u c = parseTerm();
+    if (c == ';') return heapOn(outer, heapOn(tab[tabn - 1], heapOn('<', 0)));
+    tab[tabn++] = c;
+    //if (run() != ';') printf("!%u(%c)",x,x), die("expected ';'");
+    c = run(); if (c != ';') printf("!%u(%c)\n",c,c), die("expected ';'");
   }
 }
 
@@ -118,20 +116,40 @@ void testCase(char *prog, char *want) {
 
 // ASCII 32.. = " !\"#$%&'()*"
 char *parenCompiler =
-  "`Y``B`CS``B`B`C``BB:C;"  // (++)
-  "``SS``B`BK``B`BK``B`B`:#`@ ;"  // [add] = \acc p -> acc p ('`':acc ++ p)
-  "``S``BS``B`BB`T`##=`T`#@=;"  // [isPre] = \h -> ('#' == h) || ('@' == h)
-  "``S``BS``B`BS``B`S``BB@\"``B`B`C`T?``C``BBB``C``BB:``C:K``CB``C:K;"  // [suffix] = \f h t -> isPre h (t undefined (\a b -> f (h:a:K) b)) (f (h:K) t)
-  "``BC``B`B@#``C``BBB@!;"  // [atom] = \r h acc t -> suffix (r . (add acc)) h t
-  "``S``BC``B`BB``B`BT``C``BBB@!`TK;"  // [sub] = \r acc -> uncurry ((r .) ([add] acc)) . r ""
-  "``S``BS``B`BB`T`#;=`T`#)=;"  // [term] = \h -> (';' == h) || (')' == h)
-  // Because of how we handle primitve functions, this is: \hxy.=#;hx(=#)xy)
-  "``S``BC``B`BB``B`BB@&`T`#(=;"  // [if3] = \hxyz.[term]hx(['(' ==]hyz)
-  "``BC``S``BS``B`C``C@'``BCT@%@$;"  // [switch] = \raht.[if3]h[pair]([sub]r)([atom]rh)at
-  "`Y``B`B`C`T?@(;"  // [parseParen] = fix \r acc s -> s undefined $ \h t -> [switch] r acc h t
-  "`Y``B`B`C`@)K``B`S``BS``B`BBT``B`B`B`BK``B`B`B`BK``C``BBB``C``BB@ `:#;;" // [parse] = fix \r pre s -> [parseParen] "" s (\p t -> p pre (\_ _ -> r (pre ++ ';':p) t))
-  "``B``C@ ``:#\nK`@*``:#>``:#IK"  // (++ "\n") . parse ">I"
-  ;
+/*
+pair x y f = f x y;
+uncurry x y = y x;
+(&) x y = y x;
+(.) x y z = x (y z);
+(||) f g x y = f x (g x y);
+(++) xs ys = xs ys (\x xt -> x : (xt ++ ys));
+add acc p = acc p (\_ _ -> ('`':(acc ++ p)));
+isPre h = h('#'(==)) || h('@'(==));
+suffix f h t = isPre h (t undefined (\a b -> f (h:(a:[])) b)) (f (h:[]) t);
+atom r h acc t = suffix (r . (add acc)) h t;
+sub r acc = uncurry ((r .) (add acc)) . r "";
+term h = h(';'(==)) || h(')'(==));
+if3 h x y z = term h x (h('('(==)) y z);
+switch r a h t = if3 h pair (sub r) (atom r h) a t;
+parseParen acc s = s undefined (\h t -> switch parseParen acc h t);
+parse pre s = parseParen "" s (\p t -> p pre (\_ _ -> parse (pre ++ (p ++ ";")) t));
+main = (++ ";") . parse ">";
+*/
+"``BCT;"
+"``BS`BB;"
+"`Y``B`CS``B`B`C``BB:C;"
+"``SS``B`BK``B`BK``B`B`:#`@\";"
+"``S``B@!`T`##=`T`#@=;"
+"``S``BS``B`BS``B`S``BB@$``B`B`C`T?``R``R``RK:``BB:``BBB``R``RK:B;"
+"``BC``B`B@%``R@#``BBB;"
+"``S``BC``B`BB``B`BT``R@#``BBB`TK;"
+"``S``B@!`T`#;=`T`#)=;"
+"``S``BC``B`BB``B`BB@(`T`#(=;"
+"``BC``S``BS``B`C``R@ @)@'@&;"
+"`Y``B`B`C`T?@*;"
+"`Y``B`B`C`@+K``B`S``BS``B`BBT``B`B`B`BK``B`B`B`BK``R``R``R``:#;K@\"``BB@\"``BBB;"
+"``R``:#;K``B@\"`@,``:#>K;"
+;
 
 char *skiCompiler =
 /*
@@ -161,42 +179,41 @@ var = sat (\c -> flip (c(';'(==)) || c(')'(==))));
 atom r = (char '(' *> (r <* char ')')) <|> (char '\\' *> (L <$> var) <*> (char '.' *> r)) <|> (V <$> var);
 apps r = (((&) <$> atom r) <*> ((\vs v x -> vs (A x v)) <$> apps r)) <|> pure id;
 expr = ((&) <$> atom expr) <*> apps expr;
-undefined x = x;
--- Last field should be undefined. We only print lambdas when debugging.
 show t = t id (\v -> v:[])(\x y -> '`':(show x ++ show y)) (\x y -> '\\' : (x : ('.' : show y)));
 fix x = x (fix x);
 unlam v = fix (\r t -> t (\x -> A (V 'K') (R x)) (\x -> x(v(==)) (V 'I') (A (V 'K') (V x))) (\x y -> A (A (V 'S') (r x)) (r y)) undefined);
 babs t = t R V (\x y -> A (babs x) (babs y)) (\x y -> unlam x (babs y));
 prog pre s = (expr <* char ';') s pre (\p -> p(\x t -> prog (pre ++ (show (babs x) ++ ";")) t));
-main s = prog "" s;
+main s = prog ">" s ++ ";";
 */
 "Y(B(CS)(B(B(C(BB:)))C));"
 "BCT;"
 "BKT;"
-"B(B@#)@\";"
-"B(C(TK))(B(B(RK))(R@$(BS(BB))));"
+"B(B@\")@!;"
+"B(C(TK))(B(B(RK))(R@#(BS(BB))));"
 "B(C(TK))T;"
-"C(BB(B@&(C(BB(B@&(B@$))))));"
-"B@'@$;"
-"B@'(@((KI));"
-"B@'(@(K);"
-"B(B(R@#))S;"
-"B@%(BT(T=));"
+"C(BB(B@%(C(BB(B@%(B@#))))));"
+"B@&@#;"
+"B@&(@'(KI));"
+"B@&(@'K);"
+"B(B(R@\"))S;"
+"B@$(BT(T=));"
 "B(BK)(B(BK)(B(BK)T));"
 "BK(B(BK)(B(BK)T));"
 "B(BK)(B(BK)(B(B(BK))(BCT)));"
 "B(BK)(B(BK)(B(BK)(BCT)));"
 "BS(BB);"
-"@%(BC(S(B@1(T(#;=)))(T(#)=))));"
-"R(@(@.@2)(B@+(S(B@+(B(@)(@,#())(R(@,#))@*)))(B(@'(@)(@,#\\)(@(@0@2)))(@)(@,#.)))));"
-"Y(B(R(@$I))(B(B@+)(B(S(B@'(B(@(T)@3)))(B(@((R(C@/)(BBB)))))));"
-"Y(S(B@'(B(@(T)@3))@4);"
-"Y(S(BC(B(C(R(RK:)(TI)))(B(B(B(:#`)))(S(BC(B(BB)(B@!)))I))))(B(B(B(:#\\)))(B(C(BB:))(B(:#.)))));"
-"BY(B(B(RI))(R(S(BC(B(BB)(B(B@/)(B(@/(@.#S))))))I)(BB(BC(B(C(T(B(@/(@.#K))@-)))(R(B(@/(@.#K))@.)(BS(B(R(@.#I))(BT(T=))))))))));"
-"Y(S(BC(B(C(R@.(T@-)))(S(BC(B(BB)(B@/)))I)))(C(BB@7)));"
-"Y(B(S(BC(C(@*@5(@,#;)))))(B(BT)(R(R(R(:#;K)(B@!(B@6@8)))(BB@!))(BBB))));"
-"@9K;"
+"@$(BC(S(B@0(T(#;=)))(T(#)=))));"
+"R(@'@-@1)(B@*(S(B@*(B(@((@+#())(R(@+#))@))))(B(@&(@((@+#\\)(@'@/@1)))(@((@+#.)))));"
+"Y(B(R(@#I))(B(B@*)(B(S(B@&(B(@'T)@2)))(B(@'(R(C@.)(BBB)))))));"
+"Y(S(B@&(B(@'T)@2))@3);"
+"Y(S(BC(B(C(R(RK:)(TI)))(B(B(B(:#`)))(S(BC(B(BB)(B@ )))I))))(B(B(B(:#\\)))(B(C(BB:))(B(:#.)))));"
+"BY(B(B(R?))(R(S(BC(B(BB)(B(B@.)(B(@.(@-#S))))))I)(BB(BC(B(C(T(B(@.(@-#K))@,)))(R(B(@.(@-#K))@-)(BS(B(R(@-#I))(BT(T=))))))))));"
+"Y(S(BC(B(C(R@-(T@,)))(S(BC(B(BB)(B@.)))I)))(C(BB@6)));"
+"Y(B(S(BC(C(@)@4(@+#;)))))(B(BT)(R(R(R(:#;K)(B@ (B@5@7)))(BB@ ))(BBB))));"
+"R(:#;K)(B@ (@8(:#>K)));"
 ;
+
 char *cat3(char *a, char *b, char *c) {
   static char buf[16384];
   strcpy(buf, a);
@@ -206,40 +223,43 @@ char *cat3(char *a, char *b, char *c) {
 }
 
 void runTests() {
-  testCase(">I\nHello, World!\n", "Hello, World!\n");
-  testCase(">" "`K``:#O``:#KK\n", "OK");
-  testCase("$I\n>I\ncompile then run", "compile then run");
+  testCase(">I;;Hello, World!\n", "Hello, World!\n");
+  testCase(">" "`K``:#O``:#KK;;", "OK");
+  testCase("$I;;>I;;compile then run", "compile then run");
   testCase(">"
 // xs ++ ys = case xs of { [] -> ys ; (x:xt) -> x : xt ++ ys }
 // f = fix \r xs ys -> xs ys (\x xt -> (:) x (r xt ys))
 //   = Y(B(CS)(B(B(C(BB(:))))C))
 // Sub-term B(B(C(BB(:))))C = \r ys -> \x xt -> (:) x (r xt ys)
     "`Y``B`CS``B`B`C``BB:C;"  // (++)
-    "`K``@ " "``:#B``:#eK" "``:#nK"
-    "\n",
+    "`K``@ " "``:#B``:#eK" "``:#nK;"
+    ";",
     "Ben");
   testCase(">"
     "`Y``B`CS``B`B`C``BB:C;"  // (++)
     "``SS``B`BK``B`BK``B`B`:#`@ ;"  // \acc p = acc p (\_ _ -> '`':acc ++ p)
-    "`@!``:#xK"
-    "\ny",
+    "`@!``:#xK;"
+    ";y",
     "`xy");
-  testCase(cat3(">", parenCompiler, ";`K```@$I#x``:#fK\n"),  // atom id 'x' "f"
+  testCase(cat3(">", parenCompiler, "`K``:#f``:#xK;;"),  // atom id 'x' "f"
+    "fx");
+  testCase(cat3(">", parenCompiler, "`K```@&I#x``:#fK;;"),  // atom id 'x' "f"
     "`fx");
-  testCase(cat3(">", parenCompiler, ";`K````@'#)``:#1K``:#2K``:#3K\n"),  // if3 ')' "1" "2" "3"
+  testCase(cat3(">", parenCompiler, "`K````@)#)``:#1K``:#2K``:#3K;;"),  // if3 ')' "1" "2" "3"
     "1");
-  testCase(cat3(">", parenCompiler, ";``B`TK`@)K\njust(one);not(two);"),  // fst . parseParen
+  testCase(cat3(">", parenCompiler, "``B`TK`@+K;;just(one);not(two);"),  // fst . parseParen
     "````just``one");
-  testCase(cat3(">", parenCompiler, "\npar(en);(t(he)(ses));K;;extra"),
-    ">I;```par`en;``t`he``ses;K\n");
+  testCase(cat3(">", parenCompiler, ";par(en);(t(he)(ses));K;;extra"),
+    ">```par`en;``t`he``ses;K;");
 }
 
 int pc(int c) { int r = putchar(c); fflush(stdout); return r; }
 int main(int argc, char **argv) {
   char program[16384];
-  strcpy(program, cat3("$", parenCompiler, "\n"));
+  strcpy(program, cat3("$", parenCompiler, ";"));
   strcat(program, skiCompiler);
-  strcat(program, ";\\x.\\y.x;\\x.x;\\x.\\y.\\z.xz(yz);\necho");
+  //strcat(program, ";\\x.\\y.x;\\x.x;\\x.\\y.\\z.xz(yz);;");
+  strcat(program, ";\\x.x;;pass");
   if (argc > 1) runTests(); else runWith(pc, program);
   return 0;
 }
