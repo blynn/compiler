067f24445f53634906fc04197793c548d9fda3f0
Paul Dersey <pdersey@gmail.com>
2020-12-20 13:48:27 -0500

    Modify marginally.hs to generate M2-Planet compatible code

--- go.sh
+++ go.sh
@@ -156,3 +156,36 @@ hex2 -f test/common_x86/ELF-i386-debug.hex2 \
 
 # Make methodically
 ./bin/marginally < methodically.hs > generated/methodically.c
+M2-Planet --architecture x86 \
+	-f functions/file.c \
+	-f functions/exit.c \
+	-f functions/malloc.c \
+	-f functions/calloc.c \
+	-f functions/file_print.c \
+	-f functions/in_set.c \
+	-f functions/numerate_number.c \
+	-f functions/match.c \
+	-f functions/require.c \
+	-f generated/methodically.c \
+	--debug \
+	-o bin/methodically.M1
+
+blood-elf -f bin/methodically.M1 --entry _start -o bin/methodically-footer.M1
+
+M1 -f test/common_x86/x86_defs.M1 \
+	-f test/common_x86/libc-core.M1 \
+	-f bin/methodically.M1 \
+	-f bin/methodically-footer.M1 \
+	--LittleEndian \
+	--architecture x86 \
+	-o bin/methodically.hex2
+
+hex2 -f test/common_x86/ELF-i386-debug.hex2 \
+	-f bin/methodically.hex2 \
+	--LittleEndian \
+	--architecture x86 \
+	--BaseAddress 0x8048000 \
+	-o bin/methodically --exec_enable
+
+# Make crossly
+./bin/methodically < crossly.hs > generated/crossly.c
