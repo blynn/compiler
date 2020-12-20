a7203c329eddb0849d1e7b2babe2b20a0848c3d4
Paul Dersey <pdersey@gmail.com>
2020-12-20 15:12:45 -0500

    Modify methodically.hs to output M2-Planet compatible code
    Stubbed out lazyDub() and dub() due to lack of long long support in
    M2-Planet

--- go.sh
+++ go.sh
@@ -189,3 +189,36 @@ hex2 -f test/common_x86/ELF-i386-debug.hex2 \
 
 # Make crossly
 ./bin/methodically < crossly.hs > generated/crossly.c
+M2-Planet --architecture x86 \
+	-f functions/file.c \
+	-f functions/exit.c \
+	-f functions/malloc.c \
+	-f functions/calloc.c \
+	-f functions/file_print.c \
+	-f functions/in_set.c \
+	-f functions/numerate_number.c \
+	-f functions/match.c \
+	-f functions/require.c \
+	-f generated/crossly.c \
+	--debug \
+	-o bin/crossly.M1
+
+blood-elf -f bin/crossly.M1 --entry _start -o bin/crossly-footer.M1
+
+M1 -f test/common_x86/x86_defs.M1 \
+	-f test/common_x86/libc-core.M1 \
+	-f bin/crossly.M1 \
+	-f bin/crossly-footer.M1 \
+	--LittleEndian \
+	--architecture x86 \
+	-o bin/crossly.hex2
+
+hex2 -f test/common_x86/ELF-i386-debug.hex2 \
+	-f bin/crossly.hex2 \
+	--LittleEndian \
+	--architecture x86 \
+	--BaseAddress 0x8048000 \
+	-o bin/crossly --exec_enable
+
+# Make precisely
+./bin/crossly < precisely.hs > generated/precisely.c
