8522262ffc2208d44e36cfcab3dac1fd8a42b9c3
Ben Siraphob <bensiraphob@gmail.com>
2020-12-21 01:10:25 +0700

    mescc-tools: update to 1.1.0

--- pkgs/mescc-tools.nix
+++ pkgs/mescc-tools.nix
@@ -1,12 +1,16 @@
 { stdenv, fetchgit, kaem }:
 
-stdenv.mkDerivation {
-  name = "mescc-tools";
-  nativeBuildInputs = [ kaem ];
+stdenv.mkDerivation rec {
+  pname = "mescc-tools";
+  version = "1.1.0";
+
   src = fetchgit {
     url = "https://git.savannah.nongnu.org/git/mescc-tools.git";
-    rev = "be437350ae1589c202e824ecdb97fe9ac6c470a3 ";
-    sha256 = "0pv61cd0zw2jb09xa30vfmf22vhh2ndjdpl3g88g27c4j4dn1f19";
+    rev = "Release_${version}";
+    sha256 = "1asvlkrvf2kk7hn8wp6p7rmsg2p3dijk2l709j4hklyljl1zj0bp";
   };
+
+  nativeBuildInputs = [ kaem ];
+
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
 }
