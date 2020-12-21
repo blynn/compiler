019eeaac725a3e42503a68b53bcb67e12a503947
Paul Dersey <pdersey@gmail.com>
2020-12-21 00:15:14 -0500

    modify crossly.hs to output M2-Planet compatible code

--- go.sh
+++ go.sh
@@ -222,3 +222,33 @@ hex2 -f test/common_x86/ELF-i386-debug.hex2 \
 
 # Make precisely
 ./bin/crossly < precisely.hs > generated/precisely.c
+M2-Planet --architecture x86 \
+	-f functions/file.c \
+	-f functions/exit.c \
+	-f functions/malloc.c \
+	-f functions/calloc.c \
+	-f functions/file_print.c \
+	-f functions/in_set.c \
+	-f functions/numerate_number.c \
+	-f functions/match.c \
+	-f functions/require.c \
+	-f generated/precisely.c \
+	--debug \
+	-o bin/precisely.M1
+
+blood-elf -f bin/precisely.M1 --entry _start -o bin/precisely-footer.M1
+
+M1 -f test/common_x86/x86_defs.M1 \
+	-f test/common_x86/libc-core.M1 \
+	-f bin/precisely.M1 \
+	-f bin/precisely-footer.M1 \
+	--LittleEndian \
+	--architecture x86 \
+	-o bin/precisely.hex2
+
+hex2 -f test/common_x86/ELF-i386-debug.hex2 \
+	-f bin/precisely.hex2 \
+	--LittleEndian \
+	--architecture x86 \
+	--BaseAddress 0x8048000 \
+	-o bin/precisely --exec_enable
