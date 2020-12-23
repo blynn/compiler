5a2466b91b7ad93957200a863138955a241e152f
Ben Siraphob <bensiraphob@gmail.com>
2020-12-23 08:25:46 +0700

    m2-planet: unstable-2020-12-15 -> unstable-2020-12-21

--- pkgs/m2-planet.nix
+++ pkgs/m2-planet.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "m2-planet";
-  version = "unstable-2020-12-15";
+  version = "unstable-2020-12-21";
 
   src = fetchFromGitHub {
     owner = "oriansj";
     repo = pname;
-    rev = "e5befc4feed411f55303c1fa014226b9d6017e29";
-    sha256 = "14v4r4c4nia3zc2r01yzlhvh0wrnh6f61xbxv7rb93fm0kmb9dfb";
+    rev = "4de11b1bc634c32c531effb32bc687aa14b602d7";
+    sha256 = "02dqbmhddk8zf615a3qsn56fq7xrkpqziw3api7pmdj8ih4k565s";
   };
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
